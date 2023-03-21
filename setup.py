3e65e46e18e33eba074c11e3985a1f061c2d2602
Olivier Lamarre <7255017+olamarre@users.noreply.github.com>
2023-03-20 22:11:27 -0400

    Fixed setup bug

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 
 ## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+## More info: http://docs.ros.org/en/jade/api/catkin/html/user_guide/setup_dot_py.html
 
-import setuptools
+from distutils.core import setup
 
-if __name__ == "__main__":
-    setuptools.setup()
-    
 setup(
     version='0.0.1',
     packages=['hello_python_ros'],
-    package_dir={'': 'src'}
+    package_dir={'': 'src'},
+    description='Simple ROS wrapper for a pure Python project using conda',
+    author='Olivier Lamarre',
+    url='https://github.com/olamarre/hello_python_ros.git',
 )
